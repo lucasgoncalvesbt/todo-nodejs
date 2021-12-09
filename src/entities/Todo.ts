022d1060cdac9fa8b511027aa2669b3d46b5a27a
Lucas Gonçalves <front.end.lb@gmail.com>
2021-12-09 13:59:00 -0300

    feat: adiciona entidade de todo

---
+++ src/entities/Todo.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('todos')
+export class Todo {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  authorName: string;
+
+  @CreateDateColumn()
+  created_At: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
