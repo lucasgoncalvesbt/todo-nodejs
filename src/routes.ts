fa4aa59da25e37a445650221857b3693a024b369
Lucas Gonçalves <front.end.lb@gmail.com>
2021-12-09 22:41:20 -0300

    feat: adiciona rota e controller de remover todo

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateTodoController } from './controllers/CreateTodoController';
+import { DeleteTodoController } from './controllers/DeleteTodoController';
 import { ListTodoController } from './controllers/ListTodoController';
 import { UpdateTodoController } from './controllers/UpdateTodoController';
 
@@ -9,9 +10,11 @@ const router = Router();
 const listTodoController = new ListTodoController();
 const createTodoController = new CreateTodoController();
 const updateTodoController = new UpdateTodoController();
+const deleteTodoController = new DeleteTodoController();
 
 router.get('/todos', listTodoController.handle);
 router.post('/todos', createTodoController.handle);
 router.put('/todos/:id', updateTodoController.handle);
+router.delete('/todos/:id', deleteTodoController.handle);
 
 export { router };
