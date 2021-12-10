5aff1f579ccf1edbea36e415a76cc2d8d8c45555
Lucas Gonçalves <front.end.lb@gmail.com>
2021-12-09 23:00:49 -0300

    feat: adiciona tratamento para que name e authorName não possa estra vazio

--- src/services/CreateTodoService.ts
+++ src/services/CreateTodoService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository } from 'typeorm';
 
+import { AppError } from '../errors/AppError';
 import { TodoRepository } from '../repositories/TodoRepository';
 
 interface ITodoRequest {
@@ -11,6 +12,14 @@ interface ITodoRequest {
 class CreateTodoService {
   async execute({ name, content, authorName }: ITodoRequest) {
     const todoRepository = getCustomRepository(TodoRepository);
+
+    if (name.trim().length === 0 || name === null) {
+      throw new AppError('Name is required', 400);
+    }
+
+    if (authorName.trim().length === 0 || authorName === null) {
+      throw new AppError('Author name is required', 400);
+    }
 
     const todo = todoRepository.create({
       name,
