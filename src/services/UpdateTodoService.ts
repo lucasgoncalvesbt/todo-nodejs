8e678438169133294f133834506595e7600a6a8b
Lucas Gonçalves <front.end.lb@gmail.com>
2021-12-10 02:58:07 -0300

    feat: adiciona validação de dados ao alterar um todo

--- src/services/UpdateTodoService.ts
+++ src/services/UpdateTodoService.ts
@@ -13,6 +13,19 @@ class UpdateTodoService {
   async execute(id: string, { name, content, authorName }: ITodoRequest) {
     const todoRepository = getCustomRepository(TodoRepository);
 
+    console.log({ id, name, content, authorName });
+
+    if (name !== undefined && (name.trim().length === 0 || name === '')) {
+      throw new AppError('Name is required', 400);
+    }
+
+    if (
+      authorName !== undefined &&
+      (authorName.trim().length === 0 || authorName === '')
+    ) {
+      throw new AppError('Author name is required', 400);
+    }
+
     const todoToUpdate = await todoRepository.findOne({ id });
 
     if (!todoToUpdate) {
